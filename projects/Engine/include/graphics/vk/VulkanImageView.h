dd396983e7f0a4f3a4304691a0c1029bce8534ef
nickclark2016 <nicholaus.clark115@gmail.com>
2019-05-14 19:09:29 -0400

    Fixed indentation

--- projects/Engine/include/graphics/vk/VulkanImageView.h
+++ projects/Engine/include/graphics/vk/VulkanImageView.h
@@ -7,18 +7,18 @@
 
 class VulkanImageView final : public IImageView
 {
-public:
-	explicit VulkanImageView(IGraphicsContext* aContext);
-	VulkanImageView(const VulkanImageView&) = delete;
-	VulkanImageView(VulkanImageView&&) noexcept = delete;
-	~VulkanImageView() override;
+	public:
+		explicit VulkanImageView(IGraphicsContext* aContext);
+		VulkanImageView(const VulkanImageView&) = delete;
+		VulkanImageView(VulkanImageView&&) noexcept = delete;
+		~VulkanImageView() override;
 
-	VulkanImageView& operator=(const VulkanImageView&) = delete;
-	VulkanImageView& operator=(VulkanImageView&&) noexcept = delete;
-	void construct(const ImageViewCreateInfo&) override;
-private:
-	VkImageView mImageView{};
-	IGraphicsContext* mContext;
+		VulkanImageView& operator=(const VulkanImageView&) = delete;
+		VulkanImageView& operator=(VulkanImageView&&) noexcept = delete;
+		void construct(const ImageViewCreateInfo&) override;
+	private:
+		VkImageView mImageView{};
+		IGraphicsContext* mContext;
 };
 
 #endif // vulkanimageview_h__
