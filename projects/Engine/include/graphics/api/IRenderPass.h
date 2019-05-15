44cc81417a945dba9e107bae0d5fe430dcd39d09
nickclark2016 <nicholaus.clark115@gmail.com>
2019-05-15 18:38:56 -0400

    Added api for render pass and framebuffer

---
+++ projects/Engine/include/graphics/api/IRenderPass.h
@@ -0,0 +1,73 @@
+#ifndef irenderpass_h__
+#define irenderpass_h__
+
+#include <graphics/AccessFlags.h>
+#include <graphics/AttachmentOperations.h>
+#include <graphics/DataFormat.h>
+#include <graphics/DependencyFlags.h>
+#include <graphics/PipelineStageFlags.h>
+#include <graphics/api/IGraphicsContext.h>
+#include <graphics/api/IImage.h>
+
+#include <vector>
+
+struct AttachmentDescription
+{
+	EDataFormat format;
+	EImageSampleFlagBits samples;
+	EAttachmentLoadOperation load;
+	EAttachmentStoreOperation store;
+	EAttachmentLoadOperation loadStencil;
+	EAttachmentStoreOperation storeStencil;
+	EImageLayout initial;
+	EImageLayout final;
+};
+
+struct AttachmentReference
+{
+	uint32_t index;
+	EImageLayout layout;
+};
+
+struct SubPassDescription
+{
+	std::vector<AttachmentReference> inputs;
+	std::vector<AttachmentReference> colors;
+	std::vector<AttachmentReference> resolve;
+	std::vector<AttachmentReference> depthStencil;
+	std::vector<uint32_t> preserve;
+};
+
+struct SubPassDependency
+{
+	uint32_t srcSubPass;
+	uint32_t dstSubPass;
+	PipelineStageFlags srcStages;
+	PipelineStageFlags dstStages;
+	AccessFlags srcAccess;
+	AccessFlags dstAccess;
+	DependencyFlags dependencyFlags;
+};
+
+struct RenderPassCreateInfo
+{
+	std::vector<AttachmentDescription> attachments;
+	std::vector<SubPassDescription> descriptions;
+	std::vector<SubPassDependency> dependencies;
+};
+
+class IRenderPass
+{
+public:
+	explicit IRenderPass(IGraphicsContext* aContext);
+	IRenderPass(const IRenderPass&) = delete;
+	IRenderPass(IRenderPass&&) noexcept = delete;
+	virtual ~IRenderPass() = delete;
+
+	IRenderPass& operator=(const IRenderPass&) = delete;
+	IRenderPass& operator=(IRenderPass&&) noexcept = delete;
+
+	virtual void construct(const RenderPassCreateInfo&) = 0;
+};
+
+#endif // irenderpass_h__
