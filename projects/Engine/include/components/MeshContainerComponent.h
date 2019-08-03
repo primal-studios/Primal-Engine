aec97d6ae01906a84a26ea4db9bb66ab46d519bf
Roderick Griffioen <roderick.griffioen@gmail.com>
2019-08-03 18:11:55 +0200

    Added the components back

--- projects/Engine/include/components/MeshContainerComponent.h
+++ projects/Engine/include/components/MeshContainerComponent.h
@@ -1,23 +1,23 @@
 #ifndef meshcontainercomponent_h__
 #define meshcontainercomponent_h__
 
-//#include "ecs/Component.h"
-//#include "graphics/Mesh.h"
-//
-//class MeshContainerComponent final : public Component
-//{
-//	public:
-//		explicit MeshContainerComponent(Mesh* aMesh)
-//			: mMesh(aMesh) {}
-//		~MeshContainerComponent() = default;
-//
-//		const Mesh* getMesh() const
-//		{
-//			return mMesh;
-//		}
-//
-//	private:
-//		Mesh* mMesh;
-//};
+#include "ecs/Component.h"
+#include "graphics/Mesh.h"
+
+class MeshContainerComponent final : public Component
+{
+	public:
+		explicit MeshContainerComponent(Mesh* aMesh)
+			: mMesh(aMesh) {}
+		~MeshContainerComponent() = default;
+
+		const Mesh* getMesh() const
+		{
+			return mMesh;
+		}
+
+	private:
+		Mesh* mMesh;
+};
 
 #endif // meshcontainercomponent_h__
--- projects/Engine/include/components/MeshRenderComponent.h
+++ projects/Engine/include/components/MeshRenderComponent.h
@@ -3,21 +3,19 @@
 
 #include "ecs/Component.h"
 
-//class MeshContainerComponent;
-//class VertexArray;
-//class MeshRenderComponent final : public Component
-//{
-//	public:
-//		MeshRenderComponent();
-//		~MeshRenderComponent();
-//
-//		void onConstruct() override;
-//
-//		void onRender() override;
-//
-//	private:
-//		MeshContainerComponent* mContainer;
-//		VertexArray* mVAO;
-//};
+class MeshContainerComponent;
+class MeshRenderComponent final : public Component
+{
+	public:
+		MeshRenderComponent();
+		~MeshRenderComponent();
+
+		void onConstruct() override;
+
+		void onRender() override;
+
+	private:
+		MeshContainerComponent* mContainer;
+};
 
 #endif // meshrendercomponent_h__
